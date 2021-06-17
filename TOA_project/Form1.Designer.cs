afa6afea3cb686583f01eceb1ff739acf4896de7
Ahmed Raza <imahmed18@gmail.com>
2021-06-17 18:18:57 +0500

    final

---
+++ TOA_project/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace TOA_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browse_Btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.analyze_Btn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // browse_Btn
+            // 
+            this.browse_Btn.Location = new System.Drawing.Point(613, 154);
+            this.browse_Btn.Name = "browse_Btn";
+            this.browse_Btn.Size = new System.Drawing.Size(105, 36);
+            this.browse_Btn.TabIndex = 0;
+            this.browse_Btn.Text = "Browse";
+            this.browse_Btn.UseVisualStyleBackColor = true;
+            this.browse_Btn.Click += new System.EventHandler(this.browse_Btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(245, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 45);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "C# Lexical Analyzer";
+            // 
+            // analyze_Btn
+            // 
+            this.analyze_Btn.Location = new System.Drawing.Point(613, 273);
+            this.analyze_Btn.Name = "analyze_Btn";
+            this.analyze_Btn.Size = new System.Drawing.Size(105, 39);
+            this.analyze_Btn.TabIndex = 3;
+            this.analyze_Btn.Text = "Analyze";
+            this.analyze_Btn.UseVisualStyleBackColor = true;
+            this.analyze_Btn.Click += new System.EventHandler(this.analyze_Btn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 93);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(511, 334);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.analyze_Btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.browse_Btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browse_Btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button analyze_Btn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ TOA_project/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace TOA_project
+{
+    public partial class Form1 : Form
+    {
+
+        int i = 0;
+        private static String input = "";
+        MatchCollection stringarr;
+        private static String[] tokens;
+        private static String[] separators = { ";", "{", "}", ">", "<", "+", "-", "|", "&", "`" };
+        private static String[] operators = { "()", "[]", "&&", "||", "++", "--", "==", "=<", "=>", "!=", "*", "/", "%", "(", ")", "[", "]", ",", "=" };
+
+        Regex integer = new Regex("^[+|-]?[0-9]+$");
+        Regex decimals = new Regex("^[+|-]?[0-9]*[.][0-9]+$");
+        Regex strings = new Regex("[\"].*[\"]");
+        Regex characters = new Regex("^[\'].?[\']$");                          
+        Regex identifier = new Regex("^[a-zA-Z][0-9|a-zA-Z|_]*");
+        Regex comments = new Regex("[/*].*[*/]");
+        //Regex identifier = new Regex(@"[a-zA-Z]\w*");
+        Regex seperatorss = new Regex("[;]|[{]|[}]|[|]|[&]|[`]");
+        Regex operatorss = new Regex("^[()]|[[][]]|[&&]|[,]|[||]|[,]|[+]{2}|[-]{2}|[=]{2}|[<]|[=>]|[!=]|[=]|[*]|[/]|[%]|[(]|[)]|[[]|[]]|[,]|[=]|[+]|[-]|[>]|[<]$");
+
+        Regex keywords = new Regex("abstract|as|base|bool|break|by|byte|case|catch|char|checked|" +
+            "class|const|continue|decimal|default|delegate|do|double|descending|explicit|event|" +
+            "extern|else|enum|false|finally|fixed|float|for|foreach|from|goto|group|if|implicit|" +
+            "int|interface|internal|into|is|lock|long|new|null|namespace|object|operator|out|" +
+            "override|orderby|params|private|protected|public|readonly|ref|return|switch|struct|" +
+            "sbyte|sealed|short|sizeof|stackalloc|static|string|select|this|throw|true|try|" +
+            "typeof|uint|ulong|unchecked|unsafe|ushort|using|var|virtual|volatile|void|while|where|yield"); 
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void browse_Btn_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    input = System.IO.File.ReadAllText(file);
+
+                    richTextBox1.Text = System.IO.File.ReadAllText(file);
+                }
+                catch (System.IO.IOException) { }
+            }
+        }
+
+        private void analyze_Btn_Click(object sender, EventArgs e)
+        {
+            codePrep();
+            
+        }
+
+        void codePrep()
+        {
+            richTextBox1.Text = "";
+
+            stringarr = strings.Matches(input);
+            input = Regex.Replace(input, "[\"].*[\"]", "__");
+            input = Regex.Replace(input, @"(@(?:""[^""]*"")+|""(?:[^""\n\\]+|\\.)*""|'(?:[^'\n\\]+|\\.)*')|//.*|/\*(?s:.*?)\*/", "");
+            //input = Regex.Replace(input, @"[/*](.|[\n])*[*/]", "");
+
+            for (int i = 0; i < operators.Length; i++)
+                input = input.Replace(operators[i], " " + operators[i] + " ");
+
+            for (int i = 0; i < separators.Length; i++)
+                input = input.Replace(separators[i], " " + separators[i] + " ");
+
+            input = input.Replace("+  +", "++");
+            input = input.Replace("-  -", "--");
+            input = input.Replace(">  =", ">=");
+            input = input.Replace("<  =", "<=");
+            input = input.Replace("|  |", "||");
+            input = input.Replace("&  &", "&&");
+
+            var copyinput = new StringBuilder(input);
+            for (int i = 0; i < copyinput.Length; i++)
+            {
+                if (copyinput[i] == '\n')
+                    copyinput[i] = '$';
+            }
+
+            copyinput = copyinput.Replace("\n", String.Empty);
+            copyinput = copyinput.Replace("\r", String.Empty);
+            copyinput = copyinput.Replace("\t", String.Empty);
+            copyinput = copyinput.Replace("$", "");
+            copyinput = copyinput.Replace("~", "");
+            //copyinput = copyinput.Replace("\" \"", "sub");
+            //copyinput = copyinput.Replace("\' \'", "subch");
+            tokens = copyinput.ToString().Split(' ');
+
+            richTextBox1.Text = "";
+            foreach (var token in tokens)
+            {
+                analyze(token);
+            }
+
+        }
+
+        void analyze(string x)
+        {
+
+            if(x.Equals("__"))
+            {
+                richTextBox1.AppendText(stringarr.ElementAt(i).ToString() + " -> String\n");
+                i++;
+            }
+            else if (integer.IsMatch(x))
+            {
+                richTextBox1.AppendText(x + " -> Integer\n");
+            }
+            else if (decimals.IsMatch(x))
+            {
+                richTextBox1.AppendText(x + " -> Floating Value\n");
+            }
+
+            else if (seperatorss.IsMatch(x))
+                richTextBox1.AppendText(x + " -> Seperator\n");
+
+            else if (operatorss.IsMatch(x))
+                richTextBox1.AppendText(x + " -> Operator\n");
+
+            else if (strings.IsMatch(x))
+            {
+                
+                richTextBox1.AppendText(x+ " -> String\n");
+            }
+
+            else if (characters.IsMatch(x))
+            {
+                richTextBox1.AppendText(x + " -> Character\n");
+            }
+
+            else if (keywords.IsMatch(x))
+                richTextBox1.AppendText(x + " -> KeyWord\n");
+
+            else if (x.Equals("\n") || x.Equals("\r") || x.Equals("\r\n") || x.Equals("") || x.Equals(" "))
+            {
+
+            }
+
+            else if(identifier.IsMatch(x))
+            {
+                richTextBox1.AppendText(x + " -> Identifier\n");
+            }
+
+            else
+            {
+                richTextBox1.AppendText(x + " -> Invalid token\n");
+            }
+
+        }
+    }
+}
